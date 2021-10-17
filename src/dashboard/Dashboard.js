dfad4dbc65e1bd964a264c99651b495f6e68593a
Karthik Kumar <jkkr.1996@gmail.com>
2021-10-17 14:08:29 +0530

    Add: Map recentering logic

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -1,33 +1,35 @@
-import schoolLogo from '../images/school-logo.png'
-import collegeLogo from '../images/college-logo.png'
+import schoolLogo from "../images/school-logo.png";
+import collegeLogo from "../images/college-logo.png";
 import { useEffect } from "react";
 import * as rawData from "../data/Data.json";
 
 const Data = rawData.default;
 
 function Dashboard(props) {
-  useEffect(() => {
-    console.log(Data);
-  }, []);
 
   useEffect(() => {
     if (props.isMapLoaded) {
-     let markerData =[{
-        geoCodes: Data.data[0].geoCodes,
-        picture: {
-          src: schoolLogo,
-          height: "30px",
-          width: "105px"
-        }
-      },{
-        geoCodes: Data.data[1].geoCodes,
-        picture: {
-          src: collegeLogo,
-          height: "30px",
-          width: "105px"
-        }
-      }]
-      props.renderGraphicsLayer( markerData);
+      let markerData = [
+        {
+          geoCodes: Data.data[0].geoCodes,
+          picture: {
+            src: schoolLogo,
+            height: "65px",
+            width: "250px",
+          },
+        },
+        {
+          geoCodes: Data.data[1].geoCodes,
+          picture: {
+            src: collegeLogo,
+            height: "65px",
+            width: "250px",
+          },
+        },
+      ];
+      const geoCodes = Data.data.map((dataItem) => dataItem.geoCodes);
+      props.renderGraphicsLayer(markerData);
+      props.recenterMap(geoCodes);
     }
   }, [props.isMapLoaded]);
 
--- src/dashboard/ESRIMap.js
+++ src/dashboard/ESRIMap.js
@@ -1,7 +1,11 @@
 import React, { useRef, useEffect, useState } from "react";
 import MapView from "@arcgis/core/views/MapView";
 import Map from "@arcgis/core/Map";
-import Graphic from "@arcgis/core/Graphic"
+import Graphic from "@arcgis/core/Graphic";
+
+import Point from "@arcgis/core/geometry/Point";
+import Polygon from "@arcgis/core/geometry/Polygon";
+import * as webMercatorUtils from "@arcgis/core/geometry/support/webMercatorUtils";
 
 import Dashboard from "./Dashboard";
 
@@ -9,7 +13,7 @@ import "./ESRIMap.css";
 
 function ESRIMap(props) {
   const mapDiv = useRef(null);
-  const [isMapLoaded, setIsMapLoaded] = useState(false)
+  const [isMapLoaded, setIsMapLoaded] = useState(false);
 
   let mapObjRef = useRef(null);
 
@@ -21,7 +25,7 @@ function ESRIMap(props) {
        * Initialize application
        */
 
-       mapObjRef.current = new Map({
+      mapObjRef.current = new Map({
         basemap: "hybrid",
         layers: [],
       });
@@ -32,38 +36,93 @@ function ESRIMap(props) {
         zoom: 3,
         map: mapObjRef.current,
       });
-      setIsMapLoaded(true)
+      setTimeout(function () {
+        setIsMapLoaded(true);
+    }, mapViewObjRef.current.spatialReferenceWarningDelay);
     }
   }, []);
 
-  const addGraphicsMarkerLayer = (markerData) =>{
-    let customGraphicsList = []
-    markerData.forEach(location=>{
+  const recenterMap = (locations, zoomLevel) => {
+    if (mapViewObjRef.current && locations && locations.length > 0) {
+      if (locations.length > 1 || zoomLevel) {
+        const allCoordinatesArea = [];
+        locations.forEach((element) => {
+          const latLong = webMercatorUtils.lngLatToXY(
+            element.longitude,
+            element.latitude
+          );
+          allCoordinatesArea.push(latLong);
+        });
+        const globalePolygon = new Polygon({
+          type: "polygon",
+          rings: allCoordinatesArea,
+          spatialReference: mapViewObjRef.current.spatialReference,
+        });
+        const graphic = new Graphic({
+          geometry: globalePolygon,
+        });
+        mapViewObjRef.current.extent = graphic.geometry.extent;
+        mapViewObjRef.current.zoom = zoomLevel
+          ? zoomLevel
+          : mapViewObjRef.current.zoom - 1;
+      } else {
+        let options = {
+          duration: 1000,
+          animate: true,
+          easing: "ease",
+        };
+        let pt = new Point({
+          longitude: locations[0].longitude,
+          latitude: locations[0].latitude,
+        });
+        mapViewObjRef.current.goTo(
+          {
+            target: pt,
+            zoom: 14,
+          },
+          options
+        );
+      }
+    }
+  };
+
+  const addGraphicsMarkerLayer = (markerData) => {
+    let customGraphicsList = [];
+    let geoCodes = []
+    markerData.forEach((location) => {
       const point = {
         type: "point", // autocasts as new Point()
         longitude: location.geoCodes.longitude,
-        latitude: location.geoCodes.latitude
+        latitude: location.geoCodes.latitude,
       };
-  
+
       // Create a symbol for drawing the point
       const markerSymbol = {
-        type: "picture-marker",  // autocasts as new PictureMarkerSymbol()
+        type: "picture-marker", // autocasts as new PictureMarkerSymbol()
         url: location.picture.src,
         width: location.picture.width,
-        height: location.picture.height
+        height: location.picture.height,
       };
-  
+
       // Create a graphic and add the geometry and symbol to it
       const pointGraphic = new Graphic({
         geometry: point,
-        symbol: markerSymbol
+        symbol: markerSymbol,
       });
-      customGraphicsList.push(pointGraphic)
-    })
+      customGraphicsList.push(pointGraphic);
+    });
     mapViewObjRef.current.graphics.addMany(customGraphicsList);
-  }
+  };
 
-  return <div className="mapDiv" ref={mapDiv}><Dashboard isMapLoaded={isMapLoaded} renderGraphicsLayer = {addGraphicsMarkerLayer} /></div>;
+  return (
+    <div className="mapDiv" ref={mapDiv}>
+      <Dashboard
+        isMapLoaded={isMapLoaded}
+        renderGraphicsLayer={addGraphicsMarkerLayer}
+        recenterMap={recenterMap}
+      />
+    </div>
+  );
 }
 
 export default ESRIMap;
